752538a1f6f69bc28377d6f94ccbb98e4c8fd197
Daniel Udechukwu <danieludechukwu117@gmail.com>
2022-09-09 00:51:06 +0100

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,10 +6,10 @@
  * Return: Always 0 (Success)
  */
 
-int main(void)
-{
+int main(void){
+	char myFirstProgram[] = "\"Programing is like building a multilingual puzzle\n";
 
-puts("\"Programming is like building a multilingual puzzle \n");
-
-return (0);
+	puts(myFirstProgram);
+	
+	return (0);
 }
