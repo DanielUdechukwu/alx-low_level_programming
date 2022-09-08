7c8597fd12addd90a85195184841da7ac722ad0a
Daniel Udechukwu <danieludechukwu117@gmail.com>
2022-09-09 00:40:53 +0100

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
+/*
+ * Writing my actual C program.
+ * 
+ */
+
+
 int main()
 {
-puts("Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle\n");
 return (0);
 }
