aad99b47ee63730b389483d6a8a6b818be2874b8
Daniel Udechukwu <danieludechukwu117@gmail.com>
2022-09-09 14:31:08 +0100

    Counting alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char c;
+
+for (c = 'a'; c <= 'z'; c++)
+{
+	printf("%c\n", c);
+}
+
+return (0);
+}
