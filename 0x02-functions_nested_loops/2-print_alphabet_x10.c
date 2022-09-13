6bd1bb145bd4c150cfe977ade50bde341c15e341
Daniel Udechukwu <danieludechukwu117@gmail.com>
2022-09-13 19:00:28 +0100

    Printing alphabets x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * print_alpha_x10 - Prints alphabet ten times
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet_x10(void)
+{
+	int alpha10, num;
+	for (num = 0; num < 10; num++)
+	{
+		for (alpha10 = 'a'; alpha10 <= 'z'; alpha10++)
+		{
+			_putchar(alpha10);
+		}
+		_putchar('\n');
+	}
+}
