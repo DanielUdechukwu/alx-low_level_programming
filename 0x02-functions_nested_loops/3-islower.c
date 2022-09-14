ef2d12714c821b446bd1cfd32c6fda075549f514
Daniel Udechukwu <danieludechukwu117@gmail.com>
2022-09-14 02:07:55 +0100

    Testing

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -9,7 +9,7 @@
 
 int _islower(int c)
 {
-	if (c >= 'a' && c <= 'z')
+	if (islower(c))
 	{
 		return (1);
 	}
@@ -17,5 +17,16 @@ int _islower(int c)
 	{
 		return (0);
 	}
+
+
+	/*if (c >= 'a' && c <= 'z')
+		
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}*/
 }
 
