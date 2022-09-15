240ca4eef1b38bfee382196166d2d826679982dc
Daniel Udechukwu <danieludechukwu117@gmail.com>
2022-09-15 20:58:29 +0100

    Multiplier function

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - Multiplication function
+ * @a: First variable
+ * @b: Second variable
+ *
+ * Return: Always 0 for success
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
