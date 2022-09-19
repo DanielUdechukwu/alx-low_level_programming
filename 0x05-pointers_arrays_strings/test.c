1ab25fc59f9079d7fe252e71f910bfc7f50abc02
Daniel Udechukwu <danieludechukwu117@gmail.com>
2022-09-19 21:08:54 +0100

    String length

---
+++ 0x05-pointers_arrays_strings/test.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int size = 10;
+	int aba[size] = {1, 2, 3, 4, 5};
+
+	for (int i = 0; i <= size; i++)
+	{
+		int count = 0;
+		count++;
+	}
+
+	printf(count);
+	printf("\n");
+return (0);
+}
