59a323cb3ff511f0fd66abb8d9443b675e96aa7b
Daniel Udechukwu <danieludechukwu117@gmail.com>
2022-09-22 13:05:23 +0100

    2 in 1

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,15 +1,12 @@
 #include "main.h"
 
-/*
+/**
  * _strcmp - Function compares pointers to two strings, s1 and s2.
- *
  * @s1: First string
  * @s2: Second string
  *
  * Return: Always 0.
  */
-
-
 
 int _strcmp(char *s1, char *s2)
 {
