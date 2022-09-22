62b720096821b5ecf00ffb03579697a72a3bbf08
Daniel Udechukwu <danieludechukwu117@gmail.com>
2022-09-22 12:26:34 +0100

    2 in 1

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -2,8 +2,9 @@
 
 /**
  * *_strncpy - Copies predefined string length
- * @j: Length of string
- * @i: String index
+ * @n: Number of bytes
+ * @dest: Destination string
+ * @src: String to be copied
  * Return: dest.
  */
 
@@ -13,15 +14,19 @@ char *_strncpy(char *dest, char *src, int n)
 	int i = 0, j = 0;
 
 	while (src[i++])
+	{
 		j++;
-	/*printf("%d\n", i);
-	printf("%d\n", j);*/
+	}
 
 	for (i = 0; src[i] && i < n; i++)
+	{
 		dest[i] = src[i];
+	}
 
 	for (i = j; i < n; i++)
+	{
 		dest[i] = '\0';
+	}
 
 	return (dest);
 }
