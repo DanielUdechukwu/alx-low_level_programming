ee117b07154a33d489aec629318d21f8829169ca
Daniel Udechukwu <danieludechukwu117@gmail.com>
2022-09-30 18:56:15 +0100

    Prints number of arguements

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -10,6 +10,15 @@
 
 int main(int argc, char *argv[] __attribute__ ((unused)))
 {
-	printf("%d\n", argc);
+	if (argc > 1)
+	{
+		int i, j;
+
+		for (i = 1; i < argc; i++)
+		{
+			j = i;
+		}
+		printf("%d\n", j);
+	}
 	return (0);
 }
